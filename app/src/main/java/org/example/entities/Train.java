7648c8bd775d208e1ddec89417f9d38929aedc73
Sharan-yaar <jagsharan2587@gmail.com>
2025-12-10 15:29:56 +0530

    initialisation update

--- app/src/main/java/org/example/entities/Train.java
+++ app/src/main/java/org/example/entities/Train.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class Train {
     private String trainId;
-    private String trianNo;
+    private String trainNo;
     private List<List<Integer>> seats;
     private Map<String, Time> stationTimes;
     private List<String> stations;
